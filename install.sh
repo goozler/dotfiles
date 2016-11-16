0a07648085d9fab21750ef172cab333b77c487a2
Krutov Alex <goozler@mail.ru>
2016-11-16 23:47:14 +0400

    WIP

--- install.sh
+++ install.sh
@@ -1,16 +1,6 @@
 #!/bin/bash
 cd `dirname "$0"`
 
-FILES=("gitconfig" "gitignore_global" "tmux.conf" "vimrc" "zshenv" "zshrc")
-
-for file in "${FILES[@]}"; do
-  if [ -f "$HOME/.$file" ]; then
-    mv -v $HOME/.$file $HOME/.$file.old
-  fi
-
-  ln -sfv $PWD/$file $HOME/.$file
-done
-
 if [[ `uname -n` == ubuntu* ]]; then
   # sudo apt-get install -yqq python-software-properties software-properties-common
 
@@ -26,9 +16,16 @@ if [[ `uname -n` == ubuntu* ]]; then
   sudo ln -s /usr/bin/tmux-next /usr/bin/tmux
 fi
 
-git config --global user.email "goozler@gmail.com"
-git config --global user.name "Alex Krutov"
-
 # ./install-tmux.sh
 # ./install-vim.sh
 ./install-oh-my-zsh.sh
+
+FILES=("gitconfig" "gitignore_global" "tmux.conf" "vimrc" "zshenv" "zshrc")
+for file in "${FILES[@]}"; do
+  if [ -f "$HOME/.$file" ]; then
+    mv -v $HOME/.$file $HOME/.$file.old
+  fi
+
+  ln -sfv $PWD/$file $HOME/.$file
+done
+
