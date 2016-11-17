0e114349fc5605f5dfa6aa97fd97b2ad86a91913
Krutov Alex <goozler@mail.ru>
2016-11-17 15:54:01 +0400

    Update

--- install.sh
+++ install.sh
@@ -10,16 +10,7 @@ for file in "${FILES[@]}"; do
 done
 
 if [[ $(uname -n) == ubuntu* ]]; then
-  # sudo apt-get install -yqq python-software-properties software-properties-common
-
-  # sudo add-apt-repository -y ppa:pi-rho/dev
-  # sudo apt-get update -yqq
-
-  sudo apt-get install -y curl git silversearcher-ag tmux z zsh
-
-  # tmux
-  # sudo apt-get install -yqq tmux-next=2.3~20161115~bzr3615+20-1ubuntu1~ppa0~ubuntu14.04.1
-  # sudo ln -s /usr/bin/tmux-next /usr/bin/tmux
+  sudo apt-get install -y curl git silversearcher-ag tmux zsh
 fi
 
 ./install-tmux.sh
