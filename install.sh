96415885f71dc8a239aeb31587a7e5565b22025c
Krutov Alex <goozler@mail.ru>
2016-11-18 16:35:28 +0400

    Move provisioning to Vagrant

--- install.sh
+++ install.sh
@@ -9,13 +9,6 @@ for file in "${FILES[@]}"; do
   ln -sfv $PWD/$file $HOME/.$file
 done
 
-if [[ $(uname -n) == ubuntu* ]]; then
-  # dev tools
-  sudo apt-get install -yqq build-essential cmake python python-dev python3-dev
-  # utils
-  sudo apt-get install -yqq curl git silversearcher-ag tmux zsh
-fi
-
 ./install-tmux.sh
 ./install-oh-my-zsh.sh
 ./install-vim.sh
